ca676c684d94cb32bba8b7a000c8eb406e2d1f26
Habler-code <69906239+Habler-code@users.noreply.github.com>
2023-07-15 11:01:45 +0300

    Update ObjectTracking.py
    added user prompts

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -1,3 +1,4 @@
+
 import cv2 as cv
 from pytube import YouTube
 
@@ -8,26 +9,41 @@ def download_youtube_video(youtube_link, filename):
 
 def initialize_tracker(tracker_type):
     print("Initializing the tracker...")
-    #OpenCV trackers:
+    # OpenCV trackers:
     tracker = None
-    if tracker_type == "BOOSTING":
-        tracker = cv.TrackerBoosting_create()
-    elif tracker_type == "MIL":
-        tracker = cv.TrackerMIL_create()
-    elif tracker_type == "KCF":
-        tracker = cv.TrackerKCF_create()
-    elif tracker_type == "TLD":
-        tracker = cv.TrackerTLD_create()
-    elif tracker_type == "MEDIANFLOW":
-        tracker = cv.TrackerMedianFlow_create()
-    elif tracker_type == "CSRT":
-        tracker = cv.TrackerCSRT_create()
-    elif tracker_type == "MOSSE":
-        tracker = cv.TrackerMOSSE_create()
+    try:
+        # Try to use the current tracking API
+        if tracker_type == "BOOSTING":
+            tracker = cv.TrackerBoosting_create()
+        elif tracker_type == "MIL":
+            tracker = cv.TrackerMIL_create()
+        elif tracker_type == "KCF":
+            tracker = cv.TrackerKCF_create()
+        elif tracker_type == "MEDIANFLOW":
+            tracker = cv.TrackerMedianFlow_create()
+        elif tracker_type == "CSRT":
+            tracker = cv.TrackerCSRT_create()
+        elif tracker_type == "MOSSE":
+            tracker = cv.TrackerMOSSE_create()
+    except AttributeError:
+        # If current tracking API is not available, use the legacy tracking API
+        if tracker_type == "BOOSTING":
+            tracker = cv.legacy.TrackerBoosting_create()
+        elif tracker_type == "MIL":
+            tracker = cv.legacy.TrackerMIL_create()
+        elif tracker_type == "KCF":
+            tracker = cv.legacy.TrackerKCF_create()
+        elif tracker_type == "MEDIANFLOW":
+            tracker = cv.legacy.TrackerMedianFlow_create()
+        elif tracker_type == "CSRT":
+            tracker = cv.legacy.TrackerCSRT_create()
+        elif tracker_type == "MOSSE":
+            tracker = cv.legacy.TrackerMOSSE_create()
     return tracker
 
+
 def track(frame, tracker):
-    # Update the tracker with current frame
+    # Update the tracker with the current frame
     (success, box) = tracker.update(frame)
     # If the tracker successfully updated, draw a rectangle around the tracked object
     if success:
@@ -40,14 +56,14 @@ def process_video(cap, tracker):
     while True:
         ret, frame = cap.read()
 
-        # If bounding box is defined, track the object
+        # If the bounding box is defined, track the object
         if BB is not None:
             success, frame = track(frame, tracker)
 
         # Display the frame
         cv.imshow("Frame", frame)
 
-        # Wait for key press and perform actions based on the key pressed
+        # Wait for a key press and perform actions based on the key pressed
         key = cv.waitKey(1) & 0xFF
 
         # If 'c' is pressed, select ROI and initialize the tracker
@@ -63,8 +79,32 @@ def process_video(cap, tracker):
     cap.release()
     cv.destroyAllWindows()
 
-def main(youtube_link, tracker_type):
-    print("Starting the tracking...")
+def main():
+    print("YouTube Video Object Tracking")
+
+    # Prompt the user to enter the YouTube video link
+    youtube_link = input("Enter the YouTube video link: ")
+
+    # Prompt the user to choose a tracker type
+    print("\nChoose a tracker type:")
+    print("1. BOOSTING")
+    print("2. MIL")
+    print("3. KCF")
+    print("4. MEDIANFLOW")
+    print("5. CSRT")
+    print("6. MOSSE")
+    tracker_choice = int(input("Enter the corresponding number for the tracker type: "))
+    tracker_types = [
+        "BOOSTING",
+        "MIL",
+        "KCF",
+        "MEDIANFLOW",
+        "CSRT",
+        "MOSSE"
+    ]
+    tracker_type = tracker_types[tracker_choice - 1]
+
+    print("\nStarting the tracking...")
 
     filename = 'video.mp4'
     download_youtube_video(youtube_link, filename)
@@ -79,10 +119,5 @@ def main(youtube_link, tracker_type):
 
     print("Finished processing.")
 
-# Main function
-youtube_link = 'https://www.youtube.com/watch?v=-u0AINRLfM4'  # car race video
-tracker_type = "CSRT"  # replace with your choice: BOOSTING, MIL, KCF, TLD, MEDIANFLOW, CSRT, MOSSE
-main(youtube_link, tracker_type)
-
-
-
+# Run the main function
+main()
